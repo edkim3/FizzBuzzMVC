146fafc26c8cb53bf695eb573fa52db2e33b361c
edkim3 <94409377+edkim3@users.noreply.github.com>
2021-12-03 07:01:09 -0700

    Create FizzBuzz Model
    Made a FizzBuzz model

--- FizzBuzzMVC/Models/FizzBuzz.cs
+++ FizzBuzzMVC/Models/FizzBuzz.cs
@@ -7,5 +7,9 @@ namespace FizzBuzzMVC.Models
 {
     public class FizzBuzz
     {
+        public int FizzValue { get; set; }
+        public int BuzzValue { get; set; }
+        public List<string> Result { get; set; } = new();
+
     }
 }
